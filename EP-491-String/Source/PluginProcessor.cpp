b7db55555ce47bc195e481050ba7ae1e12bc2908
jshandilya <jahan01shandilya@gmail.com>
2023-04-27 00:06:24 -0400

    Edited freeze param

--- EP-491-String/Source/PluginProcessor.cpp
+++ EP-491-String/Source/PluginProcessor.cpp
@@ -261,7 +261,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout EP491StringAudioProcessor::c
     params.push_back (std::make_unique<juce::AudioParameterFloat>("REVERBDAMPING", "Reverb Damping", juce::NormalisableRange<float> { 0.0f, 1.0f, 0.01f }, 0.5f, ""));
     params.push_back (std::make_unique<juce::AudioParameterFloat>("REVERBDRY", "Reverb Dry", juce::NormalisableRange<float> { 0.0f, 1.0f, 0.01f }, 1.0f, ""));
     params.push_back (std::make_unique<juce::AudioParameterFloat>("REVERBWET", "Reverb Wet", juce::NormalisableRange<float> { 0.0f, 1.0f, 0.01f }, 0.0f, ""));
-    params.push_back (std::make_unique<juce::AudioParameterFloat>("REVERBFREEZE", "Reverb Freeze", juce::NormalisableRange<float> { 0.0f, 1.0f, 0.01f }, 0.0f, ""));
+    params.push_back (std::make_unique<juce::AudioParameterFloat>("REVERBFREEZE", "Reverb Freeze", juce::NormalisableRange<float> { 0.0f, 1.0f, 1.0f }, 0.0f, ""));
     
     // Mix params
     params.push_back(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID { "OUTPUT", 1 }, "Output", juce::NormalisableRange<float> { -30.f, 10.0f, 0.01f}, 0.0f));
